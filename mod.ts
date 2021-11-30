314a3a60df7ee689fb52a9efb11b4a29e2b4a08c
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-11-30 14:40:14 +0000

    feat: closes #3 suppressEmbeds

--- mod.ts
+++ mod.ts
@@ -1,11 +1,17 @@
 import { Bot, CreateMessage, DiscordenoMessage, FinalHelpers } from "./deps.ts";
 import { sendDirectMessage } from "./src/sendDirectMessage.ts";
+import { suppressEmbeds } from "./src/suppressEmbeds.ts";
 
 export interface BotWithHelpersPlugin extends Bot {
   helpers: FinalHelpers & {
     sendDirectMessage: (
       userId: bigint,
       content: string | CreateMessage,
+    ) => Promise<DiscordenoMessage>;
+    suppressEmbeds: (
+      bot: Bot,
+      channelId: bigint,
+      messageId: bigint,
     ) => Promise<DiscordenoMessage>;
   };
 }
@@ -16,9 +22,13 @@ export function enableHelpersPlugin(bot: Bot): BotWithHelpersPlugin {
     userId: bigint,
     content: string | CreateMessage,
   ) => sendDirectMessage(bot, userId, content);
+  // @ts-ignore we are dynamically adding this.
+  bot.helpers.suppressEmbeds = (channelId: bigint, messageId: bigint) =>
+    suppressEmbeds(bot, channelId, messageId);
 
   return bot as BotWithHelpersPlugin;
 }
 
 // EXPORT EVERYTHING HERE SO USERS CAN OPT TO USE FUNCTIONS DIRECTLY
 export * from "./src/sendDirectMessage.ts";
+export * from "./src/suppressEmbeds.ts";
